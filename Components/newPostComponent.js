f46548cd1ddf34e573b94c08501ab0b7a170736c
Archie Godfrey <archiegodfrey@Archies-MacBook-Pro.local>
2017-07-26 23:41:09 +0100

    Failed To Update Reverting

--- Components/newPostComponent.js
+++ Components/newPostComponent.js
@@ -2,15 +2,15 @@ import functions from "/Users/archiegodfrey/Desktop/GitHub/Extended-Project/Func
 import firebase from '/Users/archiegodfrey/Desktop/GitHub/Extended-Project/firebaseConfig'
 import dismissKeyboard from 'dismissKeyboard'
 import RNFetchBlob from 'react-native-fetch-blob'
-import ImagePicker from 'react-native-image-crop-picker';
 
-import {AutoGrowingTextInput} from 'react-native-autogrow-textinput';
 import React, { Component } from 'react';
 import {
   Alert,Text,View,Animated,Easing,Modal,Image,ListView, TouchableHighlight, TextInput,Button,AsyncStorage,Dimensions,KeyboardAvoidingView
 } from 'react-native';
 
 var moment = require('moment');
+var ImagePicker = require('react-native-image-picker');
+
 var firebaseApp = require("firebase/app"); require("firebase/auth"); require("firebase/database")
 
 // Prepare Blob support
@@ -29,36 +29,28 @@ class ImageContainer extends Component {
     }
   }
 
-  chooseImage() {
-    ImagePicker.openPicker({
-      width:(frame.width),
-      height:(frame.height / 2),
-      cropping: true,
-      compressImageQuality:1,
-    }).then(image => {
-      this.setState({imageSource: image.path})
-    });
-  }
+  componentWillMount() {
+    
+  } 
 
   render() {
     if (this.state.imageSource !== "null") {
       return(
-        <TouchableHighlight onPress={() => this.chooseImage()} underlayColor="#f1f1f1">
-          <Image 
-            style={{resizeMode: 'cover', height: (frame.height / 2), width: (frame.width)}}
-            source={{uri: this.state.imageSource}}/>
-        </TouchableHighlight>
+      <Image 
+        style={{resizeMode: 'cover', height: (frame.height / 2), width: (frame.width)}}
+        source={{uri: this.state.imageSource}}/>
     )
     } else {
       return(
       <View style={{marginTop:(frame.height / 40), marginBottom:(frame.height / 40),
         alignItems:'center', borderColor:'grey',borderBottomWidth:1,}}>
-        <TouchableHighlight onPress={() => this.chooseImage()} underlayColor="#f1f1f1">
+        <TouchableHighlight>
           <Text style={{fontSize:20,marginBottom:(frame.height / 40)}}>Add an Image or Video</Text>
         </TouchableHighlight>
       </View>
     )
     }
+    
   }
 }
 
@@ -85,11 +77,8 @@ class PostDetails extends Component {
             width: (frame.width / 6)}} source={{uri: this.state.avatarSource}} />
         <View style={{flexDirection:'column',marginTop:(frame.height / 80),marginLeft:(frame.height / 80)
           ,marginBottom:(frame.height / 160)}}> 
-          <AutoGrowingTextInput style={{fontSize:22, height:(frame.height / 20)}} 
-            placeholder={'Title'}
-             maxHeight={200}
-            minHeight={45}
-            />
+          <TextInput style={{fontSize:22, height:(frame.height / 20)}} 
+            placeholder={'Title'}/>
           <View style={{alignSelf:'flex-end',flexDirection:'row', marginTop: (frame.height / 80), 
             marginBottom: (frame.height / 40), marginRight:(frame.width / 10)}} >
             <Text style={{fontSize:16,color:'grey'}}>
@@ -104,14 +93,17 @@ class PostDetails extends Component {
 }
 
 class DescriptionContainer extends Component {
+  componentWillMount() {
+    
+  }
+
   render() {
     return(
       <View style={{borderColor:'grey',borderBottomWidth:1,
       marginTop: (frame.height / 80), marginLeft:(frame.width / 10), marginRight:(frame.width / 10)}} >
-        <AutoGrowingTextInput style={{fontSize:20, height:(frame.height / 20)}}
+        <TextInput style={{fontSize:20, height:(frame.height / 20)}}
           placeholder={'Description'}
-           maxHeight={200}
-          minHeight={45}
+          multiline={true}
           maxLength={300}/>
       </View>
       
@@ -122,119 +114,33 @@ class DescriptionContainer extends Component {
 class Footer extends Component {
   constructor (props) {
     super(props);
-    this.state = {
-      day:"",
-      month:"",
-      year:"",
-      time:"",
-    }
+    this.state = {date:"2016-05-15"}
   }
 
-  checkAllDates() {
-    if (this.state.day !== "") {
-      if (this.state.month !== "") {
-        if (this.state.year !== "") {
-          if (this.state.time !== "") {
-            if (this.state.year.length === 4) {
-              if (this.state.year < moment().format('YYYY')) {
-                this.setState({year: ""})
-                alert('Must expire in the future!') 
-              } else if (this.state.year = moment().format('YYYY')) { //If it is this year, days matter
-                if (this.state.month >= moment().format('MM')) {
-                  if (this.state.day >= moment().format('DD')) {
-                    if (this.state.time.length == 5) {
-                      if (moment().format('HH:mm') > this.state.time) {
-                        this.setState({time: ""}) 
-                        alert('Must expire in the future!')
-                      } else {
-                        dismissKeyboard();
-                      }
-                    }
-                  } else {
-                    this.setState({day: ""})
-                    alert('Must expire in the future!')
-                  }
-                } else {
-                  this.setState({month: ""})
-                  alert('Must expire in the future!') 
-                }
-              }  else {
-                if (this.state.day !== "") {
-                  if (this.state.month !== "") {
-                    if (this.state.time.length == 5) {
-                      if (moment().format('HH:mm') > this.state.time) {
-                        this.setState({time: ""}) 
-                        alert('Must expire in the future!')
-                      } else {
-                        dismissKeyboard();
-                      }
-                    }
-                  }
-                }
-              }
-            } 
-          }
-        }
-      }
-    }
+  componentWillMount() {
+    const { DATE } = this.props;
   }
-
-  checkYear(year) {
-    this.setState({year: year})
-    if (year.length === 4) {
-      if (year < moment().format('YYYY')) {
-        this.setState({year: ""})
-        alert('Must expire in the future!') 
-      } 
-    }  
-  } 
-
-  checkTime(time) {
-    this.setState({time: time})
-    temp = time.replace(':','')
-    if ((temp.length > 1) && (temp.length < 3)) {
-      this.setState({time: temp.concat(':')})
-    }   
-  } 
 
   render() {
     return(
-      <View style={{alignSelf:'center', marginTop: (frame.height / 80),flexDirection:'column'}}>
-        <Text style={{fontSize:12}}>Expiration Date</Text>
-        <View style={{alignSelf:'center',flexDirection:'row'}}>
-          <TextInput style={{fontSize:20, height:(frame.height / 20), width:(frame.width / 10), marginLeft:(frame.width / 40)}}
-          placeholder={moment().format('DD')}
-          keyboardType={"number-pad"}
-          value={this.state.day}
-          onEndEditing={() => {this.checkAllDates()}}
-          onChange={(event) => this.setState({day:event.nativeEvent.text})}
+      <View style={{alignSelf:'flex-start', marginTop: (frame.height / 80),flexDirection:'row'}}>
+      <Image
+          style={{resizeMode: 'cover', height: (frame.height / 34), width:(frame.width / 18)}} 
+          source={require('/Users/archiegodfrey/Desktop/GitHub/Extended-Project/Images/ClockIcon.png')}/>
+        <TextInput style={{fontSize:20, height:(frame.height / 20), width:(frame.width / 10), marginRight:(frame.width / 40)}}
+          placeholder={'Day'}
           maxLength={2}/>
-          <TextInput style={{fontSize:20, height:(frame.height / 20), width:(frame.width / 10), marginLeft:(frame.width / 40)}}
-          placeholder={moment().format('MM')}
-          keyboardType={"number-pad"}
-          value={this.state.month}
-          onEndEditing={() => {this.checkAllDates()}}
-          onChange={(event) => this.setState({month:event.nativeEvent.text})}
+          <TextInput style={{fontSize:20, height:(frame.height / 20), width:(frame.width / 10), marginRight:(frame.width / 40)}}
+          placeholder={'Month'}
           maxLength={2}/>
-          <TextInput style={{fontSize:20, height:(frame.height / 20), width:(frame.width / 6), marginLeft:(frame.width / 40)}}
-          placeholder={moment().format('YYYY')}
-          keyboardType={"number-pad"}
-          value={this.state.year}
-          onEndEditing={() => {this.checkAllDates()}}
-          onChange={(event) => this.checkYear(event.nativeEvent.text)}
+          <TextInput style={{fontSize:20, height:(frame.height / 20), width:(frame.width / 5), marginRight:(frame.width / 40)}}
+          placeholder={'Year'}
           maxLength={4}/>
-          <TextInput style={{fontSize:20, height:(frame.height / 20), width:(frame.width / 6), marginLeft:(frame.width / 40)}}
-          placeholder={moment().format('HH:mm')}
-          keyboardType={"number-pad"}
-          value={this.state.time}
-          onEndEditing={() => {this.checkAllDates()}}
-          onChange={(event) => this.checkTime(event.nativeEvent.text)}
-          maxLength={5}/>
-        </View>
-        <TouchableHighlight style={{alignSelf:'center',paddingTop:(frame.height / 40)}}>
-          <Text style={{fontSize:20}}>Upload Post</Text>
-        </TouchableHighlight>
+          <TextInput style={{fontSize:20, height:(frame.height / 20), width:(frame.width / 5), marginRight:(frame.width / 40)}}
+          placeholder={'Time'}
+          maxLength={4}/>
       </View>
+      
     )
   }
 }
@@ -242,13 +148,12 @@ class Footer extends Component {
 export default class NewPostTemplate extends Component {
   render() {
     return(
-      <KeyboardAvoidingView style={{flex:1,backgroundColor:'white'}} behavior='position'>
+      <View style={{flex:1,backgroundColor:'white'}}>
         <ImageContainer />
         <PostDetails />
         <DescriptionContainer />    
         <Footer />  
-      </KeyboardAvoidingView>
+      </View>
     )
   }
 }
-
--- android/app/src/main/java/com/eprouter/MainApplication.java
+++ android/app/src/main/java/com/eprouter/MainApplication.java
@@ -3,7 +3,6 @@ package com.eprouter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.wix.interactable.Interactable;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new PickerPackage(),
             new ImagePickerPackage(),
             new Interactable()
       );
